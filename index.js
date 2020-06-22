ad75961fe7d8b19d5176107738007190e31ce945
Evil <decrepitasylums@gmail.com>
2020-06-22 23:38:40 +0200

    image search added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var scaruffi = new RegExp('beatles', 'i');
 var beatles = new RegExp('scaruffi', 'i');
 var me = new RegExp('(^| )link([-,!.? ]|$)','i');
 var lastSoso = -1
+var stopped = false
 
 bot.on("ready", () => {
 	console.log("hi")
@@ -56,6 +57,53 @@ function findKeyinJSON(obj,key) {
 	return result
 }
 
+function search_pics(message,query) {
+	let url = process.env.IMGBASE + "list_json.php"
+	request.get(url, async (err,res,body) => {
+		if (err) { 
+			message.channel.send(process.env.IMGBASE + " nicht erreichbar")
+			return console.log(err)
+		}
+		if (res.statusCode == 200) {
+			data = JSON.parse(body)
+			if (Array.isArray(data)) {
+				let tmp = query.split(".")
+				let query_noext = tmp[0]
+				let regex_query = new RegExp(query_noext,"i")
+				let exact = false
+				let result_data = []
+				for (let e of data) {
+					if (query === e) {
+						message.channel.send(process.env.IMGBASE + e)
+						exact = true
+						break
+					}
+					let noext = e.substring(0,e.lastIndexOf("."))
+					if (regex_query.test(noext)) {
+						result_data.push(e)
+					}
+				}
+				if (!exact) {
+					if (result_data.length > 1) {
+						let joined = result_data.join(", ")
+						if (joined.length >= 2000) {
+							joined = joined.substring(0,1997) + "..."
+						}
+						message.channel.send(joined)
+					} else if (result_data.length == 1) {
+						message.channel.send(process.env.IMGBASE + result_data[0])
+					} else {
+						message.channel.send("nichts gefunden")
+					}
+				}
+			} else {
+				message.channel.send(process.env.IMGBASE + " gab keine Liste zurück")
+				return console.log(data)
+			}
+		}
+	})
+}
+
 function getSoSo() {
 	request.get("https://oc.mymovies.dk/em2thejay/addeddate:desc", async (err,res,body) => {
 		if (err) { return console.log(err) }
@@ -97,40 +145,65 @@ function getSoSo() {
 
 bot.on("message", async message => {
 	if(message.author.bot) return;
-	if(message.content.startsWith('!roll')) {
-		const rollmessage = message.content.substring(5)
-		const args = rollmessage.split(",")
-		let num1, num2, rollit
-		if(args.length == 2 && !isNaN(parseInt(args[0])) && !isNaN(parseInt(args[1]))) { 
-			num1 = parseInt(args[0])
-			num2 = parseInt(args[1])
-			rollit = rando(num2,num1)
-		} else if (args.length > 1) {
-			num1 = rando(args.length-1,0)
-			rollit = args[num1].trim()
-		} else {
-			rollit = rando(6,1)
+	if(message.content.startsWith('!')) {
+		if(message.content.startsWith('!roll')) {
+			const rollmessage = message.content.substring(5)
+			const args = rollmessage.split(",")
+			let num1, num2, rollit
+			if(args.length == 2 && !isNaN(parseInt(args[0])) && !isNaN(parseInt(args[1]))) { 
+				num1 = parseInt(args[0])
+				num2 = parseInt(args[1])
+				rollit = rando(num2,num1)
+			} else if (args.length > 1) {
+				num1 = rando(args.length-1,0)
+				rollit = args[num1].trim()
+			} else {
+				rollit = rando(6,1)
+			}
+			return message.reply(rollit)
 		}
-		return message.reply(rollit)
-	}
-	if(scaruffi.test(message.content)) {
-		let scaruffiwords = words.beatles
-		for(let paragraph of scaruffiwords) {
-			await message.channel.send(paragraph)
+		else if(message.content.startsWith('!bild')) {
+			let splitmsg = message.content.split(" ")
+			if (splitmsg.length > 1) {
+				let query = splitmsg[1]
+				if (query.length > 2) {
+					if (!stopped) {
+						search_pics(message,query)
+						stopped = true
+						setTimeout(() => { stopped = false },3000)
+					} else {
+						await message.channel.send("nicht so schnell")
+					}
+				} else {
+					message.channel.send("suchbegriff zu kurz (min. 3 zeichen)")
+				}
+			}
 		}
-	}
-	if(me.test(message.content)) {
-		let linkquotes = words.link
-		let aany = rando(linkquotes.length,0)
-		await setTimeout(() => { message.channel.send(linkquotes[aany]) },2000)
-	}
-	if(beatles.test(message.content)) {
-		const piedo = new MessageAttachment("https://i.imgur.com/RLbYJlv.png")
-		await message.channel.send(words.scaruffi) 
-		await message.channel.send(piedo)
-	}
-	if(message.content === "schön für dich") {
-		message.channel.send(words.zocker)
+		else {
+			message.channel.send(
+				"Verfügbare Kommandos:\n`!roll` - Zufällige Zahl zwischen 1 und 6\n`!roll int x, int y` - Zufällige Zahl zwischen x und y\n`!roll str a, str b, str c...` - Zufälliger String\n`!bild suchbegriff` - Sucht nach einem Bild in /chat/"
+			)
+		}
+	} else {
+		if(scaruffi.test(message.content)) {
+			let scaruffiwords = words.beatles
+			for(let paragraph of scaruffiwords) {
+				await message.channel.send(paragraph)
+			}
+		}
+		if(me.test(message.content)) {
+			let linkquotes = words.link
+			let aany = rando(linkquotes.length,0)
+			await setTimeout(() => { message.channel.send(linkquotes[aany]) },2000)
+		}
+		if(beatles.test(message.content)) {
+			const piedo = new MessageAttachment("https://i.imgur.com/RLbYJlv.png")
+			await message.channel.send(words.scaruffi) 
+			await message.channel.send(piedo)
+		}
+		if(message.content === "schön für dich") {
+			message.channel.send(words.zocker)
+		}
 	}
 });
 
